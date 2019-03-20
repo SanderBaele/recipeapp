20b4f2a0dfe7c07cb1bbf77ef80f99adf18f8ac3
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-03-20 18:53:05 +0100

    use selective preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SelectivePreloadStrategy } from './selective-preload-strategy';
 
 const appRoutes: Routes = [
   {
     path: 'recipe',
-    loadChildren: './recipe/recipe.module#RecipeModule'
+    loadChildren: './recipe/recipe.module#RecipeModule',
+    data: { preload: true }
   },
-  // { path: '', redirectTo: 'recipe/list', pathMatch: 'full' },
+  { path: '', redirectTo: 'recipe/list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: SelectivePreloadStrategy
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
