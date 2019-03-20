468cd4247141b4d5382d447cde3c784aec79ca18
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-03-20 12:19:31 +0100

    updated cypress test to work with new routes

--- cypress/integration/myfirsttest.spec.js
+++ cypress/integration/myfirsttest.spec.js
@@ -1,7 +1,11 @@
 describe('My First Test', function() {
   it('our app runs', function() {
-    cy.visit('/');
+    cy.visit('/add-recipe');
     cy.get('button').should('be.disabled');
+  });
+
+  it('filter works', function() {
+    cy.visit('/');
     cy.get('[data-cy=filterInput]').type('sp');
     cy.get('[data-cy=recipeCard]').should('have.length', 1);
   });
