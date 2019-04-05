80086cc0bd54a29766e2c695fd5e1759605932d5
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-04-05 17:58:14 +0200

    basic first test for login dialog

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -6,4 +6,15 @@ describe('Login Page', () => {
   it('logintest', () => {
     cy.login();
   });
+
+  it('login page', () => {
+    cy.visit('/login');
+    cy.get('[data-cy=login-email]').type('recipemaster@hogent.be');
+    cy.get('[data-cy=login-password]').type('P@ssword1111');
+    cy.get('[data-cy=login-button').click();
+    // login name should be in the title balk
+    cy.contains('recipemaster@hogent.be');
+    // at last one recipe should be visible (i.e. we should have been forwarded to the recipe page)
+    cy.get('[data-cy=recipe-title]').should('be.visible');
+  });
 });
