abe6c5ef035f808316552ed2b52f1077ac216b1b
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-04-05 17:57:56 +0200

    show logged in user name in navigation (if logged in)

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticationService } from './../user/authentication.service';
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
@@ -9,12 +10,13 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
+  loggedInUser$ = this._authenticationService.user$;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private _authenticationService: AuthenticationService
+  ) {}
 }
