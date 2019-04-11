85959edd1f392cb8c13dc12fb6ad56bf5e4f2b6c
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-04-11 16:08:00 +0200

    add authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SelectivePreloadStrategy } from './selective-preload-strategy';
+import { AuthGuard } from './user/auth.guard';
 
 const appRoutes: Routes = [
   {
     path: 'recipe',
+    canActivate: [AuthGuard],
     loadChildren: './recipe/recipe.module#RecipeModule',
     data: { preload: true }
   },
